f3cb412d04bcbd76a1ab572c358f36b9eaef2b38
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-03 14:12:35 -0500

    Agrega logica de validacion de click en carta
    Se agrega una clase de valid o wrong a los botones por 500ms despues de
    haber sido clickeados, esto provee de feedback al usuario de su eleccion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 
 function Card({ data, clickedCards, setClickedCards, setHearts, setScore }) {
+  const [isCorrect, setIsCorrect] = React.useState(null);
   function handleCardClick() {
     if (clickedCards.get(data.id)) {
-      setHearts((currentHeart) => currentHeart - 1);
-      setScore(0);
-      setClickedCards(new Map());
+      setIsCorrect(false);
+
+      setTimeout(() => {
+        setHearts((currentHeart) => currentHeart - 1);
+        setScore(0);
+        setClickedCards(new Map());
+        setIsCorrect(null);
+      }, 500);
     } else {
-      const newClickedCards = new Map([...clickedCards]);
-      newClickedCards.set(data.id, true);
-      setClickedCards(newClickedCards);
-      setScore((currentScore) => currentScore + 1);
+      setIsCorrect(true);
+
+      setTimeout(() => {
+        const newClickedCards = new Map([...clickedCards]);
+        newClickedCards.set(data.id, true);
+        setClickedCards(newClickedCards);
+        setScore((currentScore) => currentScore + 1);
+        setIsCorrect(null);
+      }, 500);
     }
   }
 
   return (
-    <button className="pokemonCard" type="button" onClick={handleCardClick}>
+    <button
+      className={`pokemonCard ${
+        isCorrect !== null && isCorrect === true ? "valid" : "wrong"
+      }`}
+      type="button"
+      onClick={handleCardClick}
+    >
       <img src={data.img} alt={data.name} />
       <p>{data.name}</p>
     </button>
