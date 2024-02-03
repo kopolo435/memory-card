61055d64b2c90c3f0506b7512032f432f446c4ca
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-03 15:36:00 -0500

    Agrega elementos de header y footer y les agrega layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import GameController from "./components/GameController";
 import "./css/main.css";
 
 function App() {
-  return <GameController />;
+  return (
+    <>
+      <header>
+        <h1>Memory Game</h1>
+      </header>
+      <GameController />
+      <footer>
+        Made by
+        <a href="https://github.com/kopolo435/memory-card"> @Kopolo435</a>
+      </footer>
+    </>
+  );
 }
 
 export default App;
