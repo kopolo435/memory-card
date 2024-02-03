43a6347fc8aecf02800aef170d28c38e7360d5c0
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-03 13:56:10 -0500

    Agrega corazones que indican vidas

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import "../css/gameInfo.css";
 
 function GameInfo({ hearts, level, currentScore, maxScore }) {
   return (
     <div className="gameInfo">
-      <p>
-        Corazones:
-        <span> {hearts}</span>
-      </p>
+      <div className="heartInfo">
+        <p>Vidas</p>
+        <div className="heartsContainer">
+          <span
+            className={`material-symbols-outlined ${
+              hearts > 0 ? "fullHeart" : "emptyHeart"
+            }`}
+          >
+            favorite
+          </span>
+          <span
+            className={`material-symbols-outlined ${
+              hearts > 1 ? "fullHeart" : "emptyHeart"
+            }`}
+          >
+            favorite
+          </span>
+          <span
+            className={`material-symbols-outlined ${
+              hearts > 2 ? "fullHeart" : "emptyHeart"
+            }`}
+          >
+            favorite
+          </span>
+        </div>
+      </div>
       <p>
         Nivel:
         <span> {level}</span>
